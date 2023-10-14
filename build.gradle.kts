7698b54968ff33b666788a49779c7fd898027e87
Alexander Day <55056986+AD417@users.noreply.github.com>
2023-10-14 12:43:05 -0400

    Bump BreakInfinity to 0.2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,14 +3,14 @@ plugins {
 }
 
 group = "io.github.ad417"
-version = "1.0-SNAPSHOT"
+version = "0.1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    implementation("io.github.ad417:BreakInfinity:0.1.0")
+    implementation("io.github.ad417:BreakInfinity:0.2.0")
     implementation("org.jetbrains:annotations:24.0.0")
 
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
