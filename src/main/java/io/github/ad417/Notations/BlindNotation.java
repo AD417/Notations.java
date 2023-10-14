adad8181848a8917cdb9e2351d105a396fbad706
Alexander Day <55056986+AD417@users.noreply.github.com>
2023-10-14 14:21:05 -0400

    Implement " ".
    Added Blind Notation. Lol

---
+++ src/main/java/io/github/ad417/Notations/BlindNotation.java
@@ -0,0 +1,40 @@
+package io.github.ad417.Notations;
+
+import io.github.ad417.BreakInfinity.BigDouble;
+
+// Lol
+public class BlindNotation extends Notation {
+    public static final String name = "Blind";
+    protected static final String NEGATIVE_INFINITE = " ";
+    protected static final String INFINITE = " ";
+
+    @Override
+    public String formatNegativeDecimal(BigDouble value, int places, int placesExponent) {
+        return " ";
+    }
+
+    @Override
+    public String formatNegativeUnder1000(double value, int places) {
+        return " ";
+    }
+
+    @Override
+    public String formatUnder1000(double value, int places) {
+        return " ";
+    }
+
+    @Override
+    public String formatVerySmallNegativeDecimal(BigDouble value, int places) {
+        return " ";
+    }
+
+    @Override
+    public String formatVerySmallDecimal(BigDouble value, int places) {
+        return " ";
+    }
+
+    @Override
+    public String formatDecimal(BigDouble value, int places, int placesExponent) {
+        return " ";
+    }
+}
---
+++ src/test/java/BlindTest.java
@@ -0,0 +1,126 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import io.github.ad417.BreakInfinity.BigDouble;
+import io.github.ad417.Notations.BlindNotation;
+import io.github.ad417.Notations.Format.Settings;
+import io.github.ad417.Notations.HexNotation;
+import io.github.ad417.Notations.Notation;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+// Lol
+public class BlindTest {
+    public static Notation notation = new BlindNotation();
+
+    @BeforeEach
+    public void setDefaultSettings() {
+        Settings.lowerValueForExponentCommas = 100_000;
+        Settings.higherValueForExponentCommas = 1_000_000_000;
+        Settings.showExponentCommas = true;
+    }
+
+    @Test
+    public void testNotationName() {
+        assertEquals("Blind", BlindNotation.name);
+    }
+
+    @Test
+    public void testZeroConversion() {
+        assertEquals(" ", notation.format(BigDouble.ZERO, 0, 0));
+        assertEquals(" ", notation.format(BigDouble.ZERO, 1, 1));
+        assertEquals(" ", notation.format(BigDouble.ZERO, 2, 2));
+        assertEquals(" ", notation.format(BigDouble.ZERO, 10, 10));
+
+        BigDouble negativeZero = new BigDouble(-0);
+        assertEquals(" ", notation.format(negativeZero, 0, 0));
+        assertEquals(" ", notation.format(negativeZero, 1, 1));
+        assertEquals(" ", notation.format(negativeZero, 2, 2));
+        assertEquals(" ", notation.format(negativeZero, 10, 10));
+
+    }
+
+    @Test
+    public void testSmall() {
+        assertEquals(" ", notation.format(new BigDouble(1), 2, 2));
+        assertEquals(" ", notation.format(new BigDouble(2), 2, 2));
+        assertEquals(" ", notation.format(new BigDouble(3), 2, 2));
+        assertEquals(" ", notation.format(new BigDouble(4), 2, 2));
+        assertEquals(" ", notation.format(new BigDouble(5), 2, 2));
+        assertEquals(" ", notation.format(new BigDouble(6), 2, 2));
+        assertEquals(" ", notation.format(new BigDouble(7), 2, 2));
+        assertEquals(" ", notation.format(new BigDouble(8), 2, 2));
+        assertEquals(" ", notation.format(new BigDouble(9), 2, 2));
+        assertEquals(" ", notation.format(new BigDouble(10), 2, 2));
+        assertEquals(" ", notation.format(new BigDouble(100), 2, 2));
+    }
+
+    @Test
+    public void testRounding() {
+        BigDouble roundsUp = new BigDouble(0.6666666);
+        BigDouble roundsDown = new BigDouble(0.4);
+        BigDouble pointFive = new BigDouble(0.5);
+        assertEquals(" ", notation.format(roundsDown, 0, 0));
+
+        assertEquals(" ", notation.format(roundsUp, 0, 0));
+        assertEquals(" ", notation.format(roundsUp, 0, 1));
+        assertEquals(" ", notation.format(roundsUp, 0, 2));
+
+        assertEquals(" ", notation.format(pointFive, 0, 0));
+    }
+
+    @Test
+    public void test1000() {
+        BigDouble thousand = new BigDouble(1000);
+        assertEquals(" ", notation.format(thousand, 0, 0, 0));
+        assertEquals(" ", notation.format(thousand, 2, 0, 0));
+        assertEquals(" ", notation.format(thousand.add(6), 2, 0, 0));
+    }
+
+    @Test
+    public void testExtremelySmall() {
+        assertEquals(" ", notation.format(new BigDouble(1e-1)));
+        assertEquals(" ", notation.format(new BigDouble(1e-2)));
+        assertEquals(" ", notation.format(new BigDouble(1e-3)));
+        assertEquals(" ", notation.format(new BigDouble(1e-5)));
+        assertEquals(" ", notation.format(new BigDouble(1e-10)));
+        assertEquals(" ", notation.format(new BigDouble(1e-100)));
+        assertEquals(" ", notation.format(new BigDouble("1e-1000")));
+        assertEquals(" ", notation.format(new BigDouble("1e-1000000000000")));
+    }
+
+    @Test
+    public void testLargeWithCommas() {
+        BigDouble randomBigNum = new BigDouble("3.5826347327e289372384");
+        assertEquals(" ", notation.format(randomBigNum, 0));
+        assertEquals(" ", notation.format(randomBigNum, 1));
+        assertEquals(" ", notation.format(randomBigNum, 2));
+        assertEquals(" ", notation.format(randomBigNum, 3));
+    }
+
+    @Test
+    public void testLargeWithoutCommas() {
+        Settings.showExponentCommas = false;
+        BigDouble randomBigNum = new BigDouble("3.5826347327e289372384");
+        assertEquals(" ", notation.format(randomBigNum, 0));
+        assertEquals(" ", notation.format(randomBigNum, 1));
+        assertEquals(" ", notation.format(randomBigNum, 2));
+        assertEquals(" ", notation.format(randomBigNum, 3));
+        assertEquals(" ", notation.format(randomBigNum, 4));
+    }
+
+    @Test
+    public void testNegative() {
+        assertEquals(" ", notation.format(new BigDouble(-1), 2, 2));
+        assertEquals(" ", notation.format(new BigDouble(-2), 2, 2));
+        assertEquals(" ", notation.format(new BigDouble(-3), 2, 2));
+        assertEquals(" ", notation.format(new BigDouble(-4), 2, 2));
+        assertEquals(" ", notation.format(new BigDouble(-5), 2, 2));
+        assertEquals(" ", notation.format(new BigDouble(-6), 2, 2));
+        assertEquals(" ", notation.format(new BigDouble(-7), 2, 2));
+        assertEquals(" ", notation.format(new BigDouble(-8), 2, 2));
+        assertEquals(" ", notation.format(new BigDouble(-9), 2, 2));
+        assertEquals(" ", notation.format(new BigDouble(-10), 2, 2));
+        assertEquals(" ", notation.format(new BigDouble(-100), 2, 2));
+    }
+
+}
